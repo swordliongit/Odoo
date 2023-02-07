5c3890a0b6fa0190ab88a66e7bd56f20653f03fa
swordliongit <kilicarslan.business@gmail.com>
2023-02-07 13:08:44 +0300

    renamed:    Odoo-App-Backend/automated_action_repeated_words.py -> Odoo-App-Backend/mission_longbeach/automated_action_repeated_words.py 	renamed:    Odoo-App-Backend/server_action_button.py -> Odoo-App-Backend/mission_longbeach/server_action_button.py 	new file:   Odoo-App-Backend/mission_nfc/Contact - Link Synchronizer.py 	new file:   Odoo-App-Backend/mission_nfc/Link - Contact Synchronizer.py 	new file:   Odoo-App-Backend/mission_nfc/Link Creator Based On Contact.py 	new file:   "~$OO HATIRLATMA BELGES\304\260.rtf" 	new file:   ~$oo_learning.docx

---
+++ Odoo-App-Backend/mission_nfc/Link Creator Based On Contact.py
@@ -0,0 +1,33 @@
+# Available variables:
+#  - env: Odoo Environment on which the action is triggered
+#  - model: Odoo Model of the record on which the action is triggered; is a void recordset
+#  - record: record on which the action is triggered; may be void
+#  - records: recordset of all records on which the action is triggered in multi-mode; may be void
+#  - time, datetime, dateutil, timezone: useful Python libraries
+#  - float_compare: Odoo function to compare floats based on specific precisions
+#  - log: log(message, level='info'): logging function to record debug information in ir.logging table
+#  - UserError: Warning Exception to use with raise
+#  - Command: x2Many commands namespace
+# To return an action, assign: action = {...}
+
+#ManytoOne fields
+country_id = env['res.country'].search([('id', '=', record.id)], limit=1)
+card_owner = env['res.partner'].search([('id', '=', record.id)], limit=1)
+state_id = env['res.country.state'].search([('id', '=', record.id)], limit=1)
+#ManyToMany fields
+x_users_can_edit = env['res.users'].search([('login', '=', record.email)], limit=1)
+#create the link and map each field with the fields in the contact
+link = env['links.profile'].create({
+        'street': record.street,
+        'card_id': record.x_card_id,
+        'city': record.city,
+        'function': record.function,
+        'phone': record.phone,
+        'mobile': record.mobile,
+        'email': record.email,
+        'website': record.website,
+        'country_id': country_id.id,
+        'state': state_id.id,
+        'card_owner': card_owner.id,
+        'users_can_edit': [(6, 0, [x_users_can_edit.id])]
+        })
